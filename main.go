6477d34601e46037904f940f505049aa5c5f8565
unknown <hmuschett@gmail.com>
2020-12-16 19:12:16 +0100

    feat: add TLS server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"log"
@@ -46,5 +47,26 @@ func main() {
 	mux.HandleFunc("/api/v1/user/login", handlers.Login).Methods("GET", "POST")
 
 	log.Println("The server is lisening")
-	log.Fatal(http.ListenAndServe(configs.GetPort(), mux))
+
+	//log.Fatal(http.ListenAndServe(configs.GetPort(), mux))
+	//log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", mux))
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	srv := &http.Server{
+		Addr:         ":443",
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+
 }
