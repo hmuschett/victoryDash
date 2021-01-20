4744160753b27e5514a6789208944d7cd60f0219
unknown <hmuschett@gmail.com>
2021-01-20 10:23:56 +0100

    fear: add http\2 sopport

--- main.go
+++ main.go
@@ -55,26 +55,34 @@ func main() {
 	mux.HandleFunc("/api/v1/posorder/orders", handlers.GetPOSOrders).Methods("GET")
 	mux.HandleFunc("/api/v1/posorder/ordersmails", handlers.SendMailPOSOrders).Methods("POST")
 
-	if configs.GetEnv() == "dev" {
+	if configs.GetEnv() == "dev77" {
 		log.Fatal(http.ListenAndServe(configs.GetPort(), mux))
 	} else {
+
 		cfg := &tls.Config{
 			MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
 			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				/* 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA, */
+
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 			},
+			NextProtos: []string{"h2", "http/1.1"},
 		}
 		srv := &http.Server{
-			Addr:         ":443",
-			Handler:      mux,
-			TLSConfig:    cfg,
-			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+			Addr:      ":443",
+			Handler:   mux,
+			TLSConfig: cfg,
+			//TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
 		}
+
 		log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
 	}
 	log.Println("The server is lisening")
